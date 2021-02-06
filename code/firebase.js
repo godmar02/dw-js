367341c141fa802e76e1bd4bcbd11fff9bafbe43
godmar02 <markwgodden@gmail.com>
2021-02-06 16:28:55 +0000

    moved firebase config into seperate js

---
+++ code/firebase.js
@@ -0,0 +1,18 @@
+  // Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAIKYE0kZAcB5AzAmuiNB37BFjoiO6AY6g",
+    authDomain: "dungeon-world-45d46.firebaseapp.com",
+    projectId: "dungeon-world-45d46",
+    storageBucket: "dungeon-world-45d46.appspot.com",
+    messagingSenderId: "226498870916",
+    appId: "1:226498870916:web:7cbaa58a3a178f1afeccd1",
+    measurementId: "G-YTPKF0BTK9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const firebase = require("firebase");
+  // Required for side-effects
+  require("firebase/firestore");
