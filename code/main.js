08195956fe43e55f649e27aa674f2302b289be4f
MarkG <markgodden@uk.ibm.com>
2021-02-10 17:17:02 +0000

    scrollfix

--- code/main.js
+++ code/main.js
@@ -131,7 +131,7 @@ $(document).ready(function() {
       console.info("textarea.on() - textAreaID:", textAreaID);
     }
     textArea.height(40);
-    var newHeight = textArea.prop("scrollHeight");
+    var newHeight = textArea.prop("scrollHeight") - 20 ;
     if (debug == true) {
       console.info("textarea.on() - textArea.prop(scrollHeight):", textArea.prop("scrollHeight"));
       console.info("textarea.on() - newHeight:", newHeight);
