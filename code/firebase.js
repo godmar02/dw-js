592f1965e71e79dd040a6c3c82ea8874dc4fe235
godmar02 <markwgodden@gmail.com>
2021-02-11 21:39:14 +0000

    firebase code

--- code/firebase.js
+++ code/firebase.js
@@ -1,10 +1,11 @@
+// Require Firebase
 const firebase = require("firebase");
+
 // Required for side-effects
 require("firebase/firestore");
 
-// Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
+// Initialise Firebase
+firebase.initializeApp({
   apiKey: "AIzaSyAIKYE0kZAcB5AzAmuiNB37BFjoiO6AY6g",
   authDomain: "dungeon-world-45d46.firebaseapp.com",
   projectId: "dungeon-world-45d46",
@@ -12,8 +13,6 @@ var firebaseConfig = {
   messagingSenderId: "226498870916",
   appId: "1:226498870916:web:7cbaa58a3a178f1afeccd1",
   measurementId: "G-YTPKF0BTK9"
-};
+});
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+var db = firebase.firestore();
