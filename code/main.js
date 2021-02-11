0144774b181bebb509a80fa2bb54bf59db294947
godmar02 <markwgodden@gmail.com>
2021-02-11 21:53:28 +0000

    moved all code into one file

--- code/main.js
+++ code/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  // Require Firebase
+  const firebase = require("firebase");
+
+  // Required for side-effects
+  require("firebase/firestore");
+
+  // Initialise Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyAIKYE0kZAcB5AzAmuiNB37BFjoiO6AY6g",
+    authDomain: "dungeon-world-45d46.firebaseapp.com",
+    projectId: "dungeon-world-45d46",
+    storageBucket: "dungeon-world-45d46.appspot.com",
+    messagingSenderId: "226498870916",
+    appId: "1:226498870916:web:7cbaa58a3a178f1afeccd1",
+    measurementId: "G-YTPKF0BTK9"
+  });
+
   // variables
   var debug = true;
   var answersExpanded = 0;
