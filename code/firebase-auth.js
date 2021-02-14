95a1f06dfe1110dd225ba35f980f9495178eb422
godmar02 <markwgodden@gmail.com>
2021-02-14 21:49:28 +0000

    login user details

--- code/firebase-auth.js
+++ code/firebase-auth.js
@@ -1,25 +1,29 @@
 //Create an instance of the Google provider object:
- var provider = new firebase.auth.GoogleAuthProvider();
+var provider = new firebase.auth.GoogleAuthProvider();
 
 //Authenticate with Firebase using the Google provider object.
- firebase.auth()
- .signInWithPopup(provider)
- .then((result) => {
-   /** @type {firebase.auth.OAuthCredential} */
-   var credential = result.credential;
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
 
-   // This gives you a Google Access Token. You can use it to access the Google API.
-   var token = credential.accessToken;
-   // The signed-in user info.
-   var user = result.user;
-   console.info("signed in:", user);
- }).catch((error) => {
-   // Handle Errors here.
-   var errorCode = error.code;
-   var errorMessage = error.message;
-   // The email of the user's account used.
-   var email = error.email;
-   // The firebase.auth.AuthCredential type that was used.
-   var credential = error.credential;
-   // ...
- });
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+
+    // Set username and profile picture
+    $("#user").val(user.displayName);
+    $("#userPicture").attr("src", user.photoURL);
+
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
