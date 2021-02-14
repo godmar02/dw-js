bddf047a14f0e4f552c5c85b55af5ca3b001a46e
godmar02 <markwgodden@gmail.com>
2021-02-14 20:30:19 +0000

    Moved firebase auth config out

--- code/firebase-auth.js
+++ code/firebase-auth.js
@@ -1,14 +1,45 @@
-// Initialise Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyAIKYE0kZAcB5AzAmuiNB37BFjoiO6AY6g",
-  authDomain: "dungeon-world-45d46.firebaseapp.com",
-  databaseURL: "https://dungeon-world-45d46-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dungeon-world-45d46",
-  storageBucket: "dungeon-world-45d46.appspot.com",
-  messagingSenderId: "226498870916",
-  appId: "1:226498870916:web:7cbaa58a3a178f1afeccd1",
-  measurementId: "G-YTPKF0BTK9"
-});
+// FirebaseUI config.
+ var uiConfig = {
+   signInSuccessUrl: '<url-to-redirect-to-on-success>',
+   signInOptions: [
+     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+     firebase.auth.EmailAuthProvider.PROVIDER_ID
+   ],
+   // tosUrl and privacyPolicyUrl accept either url string or a callback
+   // function.
+   // Terms of service url/callback.
+   tosUrl: '<your-tos-url>',
+   // Privacy policy url/callback.
+   privacyPolicyUrl: function() {
+     window.location.assign('<your-privacy-policy-url>');
+   }
+ };
 
-// Get database
-var db = firebase.firestore();
+ // Initialize the FirebaseUI Widget using Firebase.
+ var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ // The start method will wait until the DOM is loaded.
+ ui.start('#firebaseui-auth-container', uiConfig);
+
+ //auth
+ var provider = new firebase.auth.GoogleAuthProvider();
+ firebase.auth()
+ .signInWithPopup(provider)
+ .then((result) => {
+   /** @type {firebase.auth.OAuthCredential} */
+   var credential = result.credential;
+
+   // This gives you a Google Access Token. You can use it to access the Google API.
+   var token = credential.accessToken;
+   // The signed-in user info.
+   var user = result.user;
+   // ...
+ }).catch((error) => {
+   // Handle Errors here.
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   // The email of the user's account used.
+   var email = error.email;
+   // The firebase.auth.AuthCredential type that was used.
+   var credential = error.credential;
+   // ...
+ });
