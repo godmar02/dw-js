482e8b7a86b69cbfa308ee4c286fdd1c22996dfa
godmar02 <markwgodden@gmail.com>
2021-02-15 20:45:59 +0000

    alignment

--- code/main.js
+++ code/main.js
@@ -616,7 +616,7 @@ $(document).ready(function() {
           // BROKEN WILL NOT SET OPTION
           console.log("loadCharacter() - chara.alignment",chara.alignment);
           console.log("loadCharacter() - #alignment",$("#alignment").val());
-          //$("#alignment").val("Chaotic");
+          $("#alignment").val("Chaotic");
           console.log("loadCharacter() - #alignment",$("#alignment").val());
           setAlignmentAttribute();
           $("#level").val(chara.level);
